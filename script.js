bdce699d86db6a049e382947e99e19777d05f3bd
melek <malekmtibaa20@gmail.com>
2024-10-18 23:44:36 +0100

    drag and drop

--- script.js
+++ script.js
@@ -5,20 +5,24 @@ function setAttribute(element, attributes) {
 }
 function addItem() {
   ulId = document.getElementById("id-box-to-add").getAttribute("value");
-  title = document.getElementById("titel").getAttribute("value");
-  console.log(title);
+  title = document.getElementById("titel").value;
   const ulItem = document.getElementById(ulId);
   const liItem = document.createElement("li");
+  const uniqueId = `item-${Date.now()}`;
+  liItem.setAttribute("draggable", "true");
+  liItem.setAttribute("ondragstart", "drag(event)");
+  liItem.setAttribute("id", uniqueId);
   liItem.classList.add("backlog-item");
   liItem.textContent = title;
   ulItem.appendChild(liItem);
-  // imageContainer.appendChild(item);
-  // img.addEventListener("load", imageLoaded);
+  closeForm();
 }
-function openForm(ulId) {
+function openForm(ulId, formTitleContent) {
   clearInput();
   const idBoxToAdd = document.getElementById("id-box-to-add");
   idBoxToAdd.value = ulId;
+  const formTitle = document.getElementById("form-title");
+  formTitle.textContent = "Add Item To " + formTitleContent;
   document.getElementById("myForm").style.display = "block";
 }
 
@@ -31,9 +35,26 @@ function clearInput() {
   document.getElementById("description").value = "";
   document.getElementById("titel").value = "";
 }
-function affectValue() {
-  // const input = document.getElementById("title");
-  // input.addEventListener("input", (event) => {
-  //   input.setAttribute("value", event.target.value);
-  // });
+
+//drag and drop
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+
+  const data = ev.dataTransfer.getData("text");
+  const draggedElement = document.getElementById(data);
+
+  if (ev.target.tagName === "UL") {
+    ev.target.appendChild(draggedElement);
+  } else {
+    console.warn("Drop target is not a valid <ul>.");
+  }
 }
