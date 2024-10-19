43ebd2da270bd4353046f784ad26505d73408e16
melek <malekmtibaa20@gmail.com>
2024-10-19 09:41:59 +0100

    store item in local storage

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function setAttribute(element, attributes) {
-  for (const key in attributes) {
-    element.setAttribute(key, attributes[key]);
-  }
-}
+localStorage.setItem("backlogItems", []);
+localStorage.setItem("progressItems", []);
+localStorage.setItem("completeItems", []);
+localStorage.setItem("holdingItems", []);
 function addItem() {
   ulId = document.getElementById("id-box-to-add").getAttribute("value");
   title = document.getElementById("titel").value;
+  description = document.getElementById("description").value;
   const ulItem = document.getElementById(ulId);
   const liItem = document.createElement("li");
   const uniqueId = `item-${Date.now()}`;
@@ -15,12 +15,15 @@ function addItem() {
   liItem.classList.add("backlog-item");
   liItem.textContent = title;
   ulItem.appendChild(liItem);
+  storeData(uniqueId);
   closeForm();
 }
-function openForm(ulId, formTitleContent) {
+function openForm(ulId, storageName, formTitleContent) {
   clearInput();
   const idBoxToAdd = document.getElementById("id-box-to-add");
+  const storageNameToAdd = document.getElementById("storage-name-to-add");
   idBoxToAdd.value = ulId;
+  storageNameToAdd.value = storageName;
   const formTitle = document.getElementById("form-title");
   formTitle.textContent = "Add Item To " + formTitleContent;
   document.getElementById("myForm").style.display = "block";
@@ -35,7 +38,24 @@ function clearInput() {
   document.getElementById("description").value = "";
   document.getElementById("titel").value = "";
 }
-
+function storeData(uniqueId) {
+  storageName = document
+    .getElementById("storage-name-to-add")
+    .getAttribute("value");
+  const obj = {
+    id: uniqueId,
+    title: title,
+    description: description,
+  };
+  let storageItems = localStorage.getItem(storageName);
+  if (storageItems) {
+    storageItems = JSON.parse(storageItems);
+  } else {
+    storageItems = [];
+  }
+  storageItems.push(obj);
+  localStorage.setItem(storageName, JSON.stringify(storageItems));
+}
 //drag and drop
 
 function allowDrop(ev) {
